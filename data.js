6dde7d9a1a2bb96ac2b19aa6109f32ab5f2b77e4
wakawakatnt <piza.pasuta.com@gmail.com>
2026-04-14 23:17:25 +0900

    Update data.js

--- data.js
+++ data.js
@@ -11,14 +11,12 @@
  *    id: 番号,
  *    title: "タイトル",
  *    imgur: "https://i.imgur.com/XXXXX.jpg",
- *    imgurPage: "https://imgur.com/XXXXX",
  *    xUrl: "https://x.com/ユーザー名/status/数字",
  *    tags: ["タグ1", "タグ2"],
  *  },
  *
  *  ・id は他と被らない数字にする
  *  ・imgur は画像の直リンク（.jpg .png 等）
- *  ・imgurPage は Imgur のページURL
  *  ・xUrl は X のポストURL
  *  ・tags は無くてもOK → tags: [],
  *
@@ -31,7 +29,6 @@ const images = [
     id: 1,
     title: "サンプル画像1",
     imgur: "https://i.imgur.com/example1.jpg",
-    imgurPage: "https://imgur.com/example1",
     xUrl: "https://x.com/example/status/111111111",
     tags: ["サンプル"],
   },
@@ -40,7 +37,6 @@ const images = [
     id: 2,
     title: "サンプル画像2",
     imgur: "https://i.imgur.com/example2.jpg",
-    imgurPage: "https://imgur.com/example2",
     xUrl: "https://x.com/example/status/222222222",
     tags: ["サンプル"],
   },
