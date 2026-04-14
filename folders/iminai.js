58234543936777b966b2533193ba187d489032a9
wakawakatnt <piza.pasuta.com@gmail.com>
2026-04-15 00:01:34 +0900

    Create iminai.js

---
+++ folders/iminai.js
@@ -0,0 +1,46 @@
+/*
+ * ===================================================
+ *  📁 フォルダ: 意味のない質問だよ
+ * ===================================================
+ *
+ *  テンプレ（コピペして追加）:
+ *
+ *  {
+ *    id: 番号,
+ *    title: "タイトル",
+ *    imgur: "https://i.imgur.com/XXXXX.jpg",
+ *    xUrl: "https://x.com/ユーザー名/status/数字",
+ *    tags: ["タグ1", "タグ2"],
+ *  },
+ *
+ * ===================================================
+ */
+
+registerFolder({
+  id: "iminai",
+  name: "意味のない質問だよ",
+  images: [
+    {
+      id: 101,
+      title: "意味のない質問だよ1",
+      imgur: "https://i.imgur.com/A7VpNQZ.png",
+      xUrl: "https://x.com/GZ8FxNhlHi45022/status/2044067234482696274",
+      tags: ["意味の無い", "意味のない質問だよ"],
+    },
+    {
+      id: 102,
+      title: "意味のない質問だよ2",
+      imgur: "https://i.imgur.com/ZZ1sI28.jpeg",
+      xUrl: "https://x.com/GZ8FxNhlHi45022/status/2044067289159663918",
+      tags: ["意味の無い", "意味のない質問だよ"],
+    },
+    {
+      id: 103,
+      title: "意味のない質問だよ3",
+      imgur: "https://i.imgur.com/3aBkv2K.jpeg",
+      xUrl: "https://x.com/GZ8FxNhlHi45022/status/2044067336685269394",
+      tags: ["意味の無い", "意味のない質問だよ"],
+    },
+    // ★ ここに追加していく ★
+  ],
+});
